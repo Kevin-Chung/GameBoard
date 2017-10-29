51a05dbcdbfcbd1237da4b20236e526b9698e24b
Cameron Moreau <cameron.cm6@gmail.com>
2017-10-28 23:10:41 -0500

    added arrows for swiping

--- android/src/com/gameboard/activities/SwipeActivity.java
+++ android/src/com/gameboard/activities/SwipeActivity.java
@@ -1,9 +1,14 @@
 package com.gameboard.activities;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.gameboard.R;
@@ -15,8 +20,30 @@ public class SwipeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_pairing_acitivty);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Fade Arrows
+        int arrows[] = {
+                R.id.imageview_arrow_1,
+                R.id.imageview_arrow_2,
+                R.id.imageview_arrow_3,
+                R.id.imageview_arrow_4,
+                R.id.imageview_arrow_5,
+                R.id.imageview_arrow_6
+        };
+
+        for (int arrowId : arrows) {
+            ImageView image = findViewById(arrowId);
+
+            ObjectAnimator alphaAnimation = ObjectAnimator.ofFloat(image, "alpha",  1f, 0.05f);
+            alphaAnimation.setDuration(1000);
+            alphaAnimation.setRepeatMode(ValueAnimator.REVERSE);
+            alphaAnimation.setRepeatCount(ValueAnimator.INFINITE);
+            alphaAnimation.start();
+        }
+
+        // Swipe listener on root
 
         View root = findViewById(android.R.id.content);
         root.setOnTouchListener(new OnSwipeGestureListener(getApplication()) {
