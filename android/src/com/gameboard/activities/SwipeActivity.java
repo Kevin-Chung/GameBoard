b7c3f56339d26401814a71886308d0b9b42b4841
Cameron Moreau <cameron.cm6@gmail.com>
2017-10-28 22:32:31 -0500

    Swiping works

--- android/src/com/gameboard/activities/SwipeActivity.java
+++ android/src/com/gameboard/activities/SwipeActivity.java
@@ -1,13 +1,13 @@
 package com.gameboard.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import com.gameboard.R;
+import com.gameboard.gestures.OnSwipeGestureListener;
 
 public class SwipeActivity extends AppCompatActivity {
 
@@ -18,12 +18,26 @@ public class SwipeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        View root = findViewById(android.R.id.content);
+        root.setOnTouchListener(new OnSwipeGestureListener(getApplication()) {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onSwipeRight() {
+                Toast.makeText(getApplicationContext(), "Swipe right", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipeLeft() {
+                Toast.makeText(getApplicationContext(), "Swipe Left", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipeTop() {
+                Toast.makeText(getApplicationContext(), "Swipe Top", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipeBottom() {
+                Toast.makeText(getApplicationContext(), "Swipe Bottom", Toast.LENGTH_SHORT).show();
             }
         });
     }
